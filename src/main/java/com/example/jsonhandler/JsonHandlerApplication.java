e2a492987c8760459b1bc06a518f58454b5fd1a3
Umaralikhon <umaralikhon@gmail.com>
2021-11-17 17:41:23 +0500

    Initial commit

--- src/main/java/com/example/jsonhandler/JsonHandlerApplication.java
+++ src/main/java/com/example/jsonhandler/JsonHandlerApplication.java
@@ -1,54 +1,61 @@
 package com.example.jsonhandler;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.json.simple.parser.ParseException;
 import org.springframework.boot.configurationprocessor.json.JSONException;
+
+import java.util.Map;
 
 
 public class JsonHandlerApplication {
 
     public static void main(String[] args) throws JSONException, ParseException, JsonProcessingException {
 
+//        String jsonStr = "{\n" +
+//                "\"name\":\"John\",\n" +
+//                "\"age\":30,\n" +
+//                "\"cars\":[\"Ford\", \"BMW\", \"Fiat\"]\n" +
+//                "}";
+
+//        String jsonStr = "{\"cars\": [\"Ford\", \"BMW\", \"Fiat\"]}";
+
         String jsonStr = "{\n" +
-                "\"name\":\"John\",\n" +
-                "\"age\":30,\n" +
-                "\"cars\":[\"Ford\", \"BMW\", \"Fiat\"]\n" +
+                "    \"countriesRoute\": {\n" +
+                "        \"Name\": \"Get List Of Countries\",\n" +
+                "        \"Description\": \"Returns all countries and associated provinces. The country_slug variable is used for country specific data\",\n" +
+                "        \"Path\": \"/countries\"\n" +
+                "    },\n" +
+                "    \"countryDayOneRoute\": {\n" +
+                "        \"Name\": \"Get List Of Cases Per Country Per Province By Case Type From The First Recorded Case\",\n" +
+                "        \"Description\": \"Returns all cases by case type for a country from the first recorded case. \",\n" +
+                "        \"Path\": \"/dayone/country/:country\"\n" +
+                "    }\n" +
                 "}";
 
 //        String jsonStr = "{\n" +
-//                "    \"countriesRoute\": {\n" +
-//                "        \"Name\": \"Get List Of Countries\",\n" +
-//                "        \"Description\": \"Returns all countries and associated provinces. The country_slug variable is used for country specific data\",\n" +
-//                "        \"Path\": \"/countries\"\n" +
-//                "    },\n" +
-//                "    \"countryDayOneRoute\": {\n" +
-//                "        \"Name\": \"Get List Of Cases Per Country Per Province By Case Type From The First Recorded Case\",\n" +
-//                "        \"Description\": \"Returns all cases by case type for a country from the first recorded case. \",\n" +
-//                "        \"Path\": \"/dayone/country/:country\"\n" +
-//                "    }\n" +
-//                "}";
+//                "    \"name\":\"John\",\n" +
+//                "    \"age\":30,\n" +
+//                "    \"cars\": [\n" +
+//                "        { \"name\":\"Ford\", \"models\":[ \"Fiesta\", \"Focus\", \"Mustang\" ] },\n" +
+//                "        { \"name\":\"BMW\", \"models\":[ \"320\", \"X3\", \"X5\" ] },\n" +
+//                "        { \"name\":\"Fiat\", \"models\":[ \"500\", \"Panda\" ] }\n" +
+//                "     ]\n" +
+//                " } ";
 
-        ObjectMapper objectMapper = new ObjectMapper();
-//        ObjectNode objectNode = (ObjectNode) objectMapper.readTree(jsonStr);
-        ObjectNode objectNode2 = (ObjectNode) objectMapper.readTree(jsonStr);
+        System.out.println("###########################################################");
+
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        JsonNode jsonNode = objectMapper.readTree(jsonStr);
+
+        System.out.println("OLD: " + jsonStr);
+
         MyJsonHandler jsonHandler = new MyJsonHandler();
+        String newStr = jsonHandler.replaceParamValue(jsonStr, "Name", "******");
 
-//        jsonHandler.replaceParamValue(objectNode, "Name", "*****");
-//        jsonHandler.replaceParamValue(objectNode, "Description", "######");
-//        jsonHandler.replaceParamValue(objectNode, "Path", "@@@@@@@");
-
-//        jsonHandler.replaceParamValue(objectNode2, "cars", "*****");
-
-//        System.out.println(objectNode.toPrettyString());
-//        System.out.println("######################");
-//        System.out.println(objectNode2.toPrettyString());
-
-        System.out.println("##################################################");
-
-        Test test = new Test(jsonStr);
-        test.parseJsonArray("cars", "***");
+        System.out.println("NEW: " + newStr);
     }
 }
 
--- src/main/java/com/example/jsonhandler/MyJsonHandler.java
+++ src/main/java/com/example/jsonhandler/MyJsonHandler.java
@@ -1,14 +1,13 @@
 package com.example.jsonhandler;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.*;
-//import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.Data;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.boot.configurationprocessor.json.JSONArray;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 
@@ -23,6 +22,8 @@ public class MyJsonHandler {
     private JSONArray jsonArray;
     private JSONObject jsonObject;
     private JSONParser jsonParser;
+    private ArrayNode arrayNode;
+    private JsonNode jsonNode;
 
     private String str = null;
     //TODO: Working code
@@ -41,37 +42,98 @@ public class MyJsonHandler {
 //        System.out.println(objectNode.toPrettyString());
 //    }
 
-    public void replaceParamValue(ObjectNode objectNode, String key, String value) throws JSONException {
-        if (objectNode.has(key)) {
-            JsonNode keyObj = objectNode.get(key);
-            if (keyObj.isArray()) {
-                System.out.println(key + " - IS ARRAY");
-            } else if (keyObj.isObject()) {
-                System.out.println(key + " - IS OBJECT");
+    public String replaceParamValue(String jsonStr, String key, String value) throws JSONException, JsonProcessingException {
+        objectMapper = new ObjectMapper();
+        jsonNode = objectMapper.readTree(jsonStr);
+
+        jsonObject = new JSONObject();
+
+        JsonNode resultValue = objectMapper.readerFor(JsonNode.class).readValue("{\"value\":\"" + value + "\"}");
+
+        if (jsonNode.isTextual()) {
+            System.out.println("STATUS: PRIMITIVE \tKEY: " + jsonNode.toString());
+        } else if (jsonNode.isInt()) {
+            System.out.println("STATUS: PRIMITIVE \tKEY: " + jsonNode.toString());
+        }
+
+        jsonNode.fields().forEachRemaining(
+                f -> {
+                    if (f.getValue().isTextual()) {
+                        System.out.println("STATUS: TEXT \tKEY: " + f.getKey() + "\t VALUE: " + f.getValue().toString());
+                        if (f.getKey().equals(key)) {
+                            f.setValue(resultValue.get("value"));
+                            System.out.println("CHANGE: TEXT \tKEY: " + f.getKey() + "\t VALUE: " + f.getValue().toString());
+                        }
+                    } else if (f.getValue().isInt()) {
+                        System.out.println("STATUS: INT \tKEY: " + f.getKey() + "\t VALUE: " + f.getValue().toString());
+                        if (f.getKey().equals(key)) {
+                            f.setValue(resultValue.get("value"));
+                            System.out.println("CHANGE: TEXT \tKEY: " + f.getKey() + "\t VALUE: " + f.getValue().toString());
+                        }
+                    } else if (f.getValue().isValueNode()) {
+                        System.out.println("STATUS: NODE \tKEY: " + f.getKey() + "\t VALUE: " + f.getValue().toString());
+                    } else if (f.getValue().isArray()) {
+                        System.out.println("STATUS: ARRAY \tKEY: " + f.getKey() + "\t VALUE: " + f.getValue().toString());
+
+                        f.getValue().forEach(
+                                t -> {
+                                    try {
+                                        replaceParamValue(t.toString(), key, value);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    } catch (JsonProcessingException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                        );
+                    } else if (f.getValue().isObject()) {
+                        System.out.println("STATUS: OBJECT \tKEY: " + f.getKey() + "\t VALUE: " + f.getValue().toString());
+
+                        try {
+                            replaceParamValue(f.getValue().toString(), key, value);
+                        } catch (JSONException | JsonProcessingException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        System.out.println("STATUS: NONE \tKEY: " + f.getKey() + "\t VALUE: " + f.getValue().toString());
+                    }
+                }
+        );
+        return jsonNode.toString();
+/*
+        while (jsonNode.fields().hasNext()) {
+            Map.Entry<String, JsonNode> nested = jsonNode.fields().next();
+        }
+
+        if (jsonNode.has(key)) {
+
+            if (jsonNode.get(key).isArray()) {
+                for (int i = 0; i < jsonNode.get(key).size(); i++) {
+                    arrayNode.insert(i, value);
+                }
+
+                objectNode.remove(key);
+                objectNode.put(key, arrayNode.toPrettyString());
+            } else if (jsonNode.get(key).isObject()) {
+                objectNode.put(key, value);
+                System.out.println("Object");
+            } else if (jsonNode.get(key) != null) {
+                objectNode.put(key, value);
             } else {
-                System.out.println(key + " - ERROR PARSE");
-            }
-        }
-        jsonArray.put(objectNode);
-
-        for (int i = 0; i < jsonArray.length(); i++) {
-            jsonArray.put(i, value);
-        }
-//            objectNode.put(key, value);
-
-//        else if(objectNode.has(key)&&objectNode.isObject()){
-//            objectNode.put(key, value);
-//        }
-
-
-        for (Iterator<Map.Entry<String, JsonNode>> it = objectNode.fields(); it.hasNext(); ) {
-            Map.Entry<String, JsonNode> nested = it.next();
-
-            if (nested.getValue().isObject()) {
-                replaceParamValue((ObjectNode) nested.getValue(), key, value);
+                System.out.println("Error parse");
             }
         }
 
-        System.out.println(objectNode.toPrettyString());
+        for (Iterator<Map.Entry<String, JsonNode>> iter = objectNode.fields(); iter.hasNext(); ) {
+            Map.Entry<String, JsonNode> nested = iter.next();
+
+            if (nested.getValue().isArray()) {
+                replaceParamValue(nested.getValue().toString(), key, value);
+            } else if (nested.getValue().isObject()) {
+                replaceParamValue(nested.getValue().toString(), key, value);
+            }
+        }
+
+        System.out.println(jsonNode.toPrettyString());*/
     }
 }
